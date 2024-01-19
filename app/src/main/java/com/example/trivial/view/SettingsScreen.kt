ce9f6a1968e69f2ae13b99002a7122862b219017
sjo <ivan.guell.7e7@itb.cat>
2024-01-19 10:17:16 +0100

    12

--- app/src/main/java/com/example/trivial/view/SettingsScreen.kt
+++ app/src/main/java/com/example/trivial/view/SettingsScreen.kt
@@ -35,6 +35,7 @@ import androidx.navigation.NavController
 @Composable
 fun SettingsScreen(navController: NavController) {
     var difficulty by remember { mutableStateOf("Medio") }
+    var type by remember { mutableStateOf("Todos") }
     var show by remember { mutableStateOf(false) }
 
 
@@ -47,7 +48,7 @@ fun SettingsScreen(navController: NavController) {
     ) {
 
         difficulty = difficultyDropDown()
-
+        type = typeDropDown()
         Button(
             modifier = Modifier
                 .padding(top = 46.dp),
@@ -60,69 +61,71 @@ fun SettingsScreen(navController: NavController) {
             Text("Ayuda")
         }
         HelpDialog(show, { show = false }, { show = false })
+
+
     }
 }
 
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun difficultyDropDown(): String {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun difficultyDropDown(): String {
 
-        var expanded by remember { mutableStateOf(false) }
-        var selectedText by remember { mutableStateOf("Dificultad") }
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf("Dificultad") }
 
-        Column(
+    Column(
+        modifier = Modifier
+            .offset(y = (10).dp)
+
+
+    ) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            enabled = false,
+            readOnly = true,
             modifier = Modifier
-                .offset(y = (10).dp)
+                .clickable { expanded = true }
+                .width(300.dp),
+
+            textStyle = TextStyle(
+                fontSize = 26.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            ),
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .padding(16.dp)
 
 
         ) {
-            OutlinedTextField(
-                value = selectedText,
-                onValueChange = { selectedText = it },
-                enabled = false,
-                readOnly = true,
-                modifier = Modifier
-                    .clickable { expanded = true }
-                    .width(300.dp),
-
-                textStyle = TextStyle(
-                    fontSize = 26.sp,
-                    color = Color.Black,
-                    textAlign = TextAlign.Center
-                ),
-            )
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-                modifier = Modifier
-                    .padding(16.dp)
-
-
-            ) {
-                listOf("Fácil", "Medio", "Difícil").forEach { difficulty ->
-                    DropdownMenuItem(text = { Text(text = difficulty) }, onClick = {
-                        expanded = false
-                        selectedText = difficulty
-                    })
-                }
+            listOf("Fácil", "Medio", "Difícil").forEach { difficulty ->
+                DropdownMenuItem(text = { Text(text = difficulty) }, onClick = {
+                    expanded = false
+                    selectedText = difficulty
+                })
             }
         }
-        return selectedText
     }
+    return selectedText
+}
 
 
-    @Composable
+@Composable
 
-    fun HelpDialog(show: Boolean, onDismiss: () -> Unit, onConfirm: () -> Unit) {
+fun HelpDialog(show: Boolean, onDismiss: () -> Unit, onConfirm: () -> Unit) {
 
-        if (show) {
-            AlertDialog(
-                onDismissRequest = { onDismiss() },
-                title = { Text("¿Como jugar?", fontSize = 20.sp) },
-                text = {
-                    Text(
-                        text = """
+    if (show) {
+        AlertDialog(
+            onDismissRequest = { onDismiss() },
+            title = { Text("¿Como jugar?", fontSize = 20.sp) },
+            text = {
+                Text(
+                    text = """
                               Se trata del ahorcado, 
                               las mecanicas son simples 
                               Solo debes introducir
@@ -141,19 +144,67 @@ fun SettingsScreen(navController: NavController) {
                                    5 fallos
 
                                """.trimIndent(),
-                        fontSize = 20.sp,
-                        textAlign = TextAlign.Start
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Start
 
+                )
+            },
+            confirmButton = {
+                TextButton(onClick = { onConfirm() }) {
+                    Text(
+                        text = "Ok!",
+                        fontSize = 20.sp
                     )
-                },
-                confirmButton = {
-                    TextButton(onClick = { onConfirm() }) {
-                        Text(
-                            text = "Ok!",
-                            fontSize = 20.sp
-                        )
-                    }
-                },
-            )
+                }
+            },
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun typeDropDown(): String {
+
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf("Generos") }
+
+    Column(
+        modifier = Modifier
+            .offset(y = (10).dp)
+
+
+    ) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            enabled = false,
+            readOnly = true,
+            modifier = Modifier
+                .clickable { expanded = true }
+                .width(300.dp),
+
+            textStyle = TextStyle(
+                fontSize = 26.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            ),
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .padding(16.dp)
+
+
+        ) {
+            listOf("Historia", "Geografia", "Cine", "Todos").forEach { type ->
+                DropdownMenuItem(text = { Text(text = type) }, onClick = {
+                    expanded = false
+                    selectedText = type
+                })
+            }
         }
     }
+    return selectedText
+}
+
